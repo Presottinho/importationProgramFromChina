4490224b6b45b6e483f333f9964531b44ea83146
Gabriel Presotto <114623358+Presottinho@users.noreply.github.com>
2023-07-02 18:47:46 +0000

    Completed System

--- itens.java
+++ itens.java
@@ -7,10 +7,40 @@ public class itens{
     List<String> itens = new ArrayList<>();
     List<Double> prices = new ArrayList<>();
 
-    public void adicionarItens(String item, double price){
+    public void addItens(String item, double price){
         itens.add(item);
         prices.add(price);
 
     }
 
+    public void showItens(double cotacao, double frete){
+        double total = 0;
+        System.out.println("-------------------------------------");
+        for(int i = 0; i < itens.size(); i++){
+            System.out.println((i + 1) + "." + itens.get(i) + " - " + "R$" + (prices.get(i) * cotacao));
+            total = total + (prices.get(i) * cotacao);
+        }
+        System.out.println("-------------------------------------");
+        System.out.println("Valor total: R$" + total + " | Frete: " + (frete * cotacao) + " | Valor total com frete: R$" + (total + (frete * cotacao)));
+        System.out.println("-------------------------------------");
+    }
+
+    public void exhibitionItem(){
+        System.out.println("-------------------------------------");
+        for(int i = 0; i < itens.size(); i++){
+            System.out.println((i + 1) + "." + itens.get(i) + " - " + "R$" + prices.get(i));
+        }
+        System.out.println("-------------------------------------");
+    }
+
+    public void editItem(int change, String newItem, double newPrice){
+        for(int i = 0; i < itens.size(); i++){
+            if((change - 1) == i){
+                itens.remove(i);
+                prices.remove(i);
+                itens.add(newItem);
+                prices.add(newPrice);
+            }
+        }
+    }
 }
--- main.java
+++ main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,9 +10,12 @@ public class main{
     public static void main(String[] Args){
 
         Scanner input = new Scanner(System.in);
-        Itens itens = new Itens();
+        itens itens = new itens();
 
         int continua = 1;
+
+        System.out.println("Digite a cotacao do Yuan: ");
+        double cotacao = input.nextDouble();
 
         do{
 
@@ -23,24 +28,39 @@ public class main{
                     String item = input.next();
                     System.out.println("Digite o preço em Yuan:");
                     double price = input.nextDouble();
-                    itens.add(item);
-                    break;
+                    itens.addItens(item, price);
+                break;
 
                 case 2:
-
-                    break;
+                    itens.exhibitionItem();
+                    System.out.println("Digite o numer o correspondente ao item que deseja editar: ");
+                    int change = input.nextInt();
+                    System.out.println("Digite o novo nome do item: ");
+                    String newItem = input.next();
+                    System.out.println("Digite o novo valor do item: ");
+                    double  newPrice = input.nextDouble();
+                    itens.editItem(change, newItem, newPrice);
+                    
+                break;
                 
                 case 3:
-                    
-                    break;
+                    System.out.println("Digite o frete em Yuan: ");
+                    double frete = input.nextDouble();
+                    itens.showItens(cotacao, frete);
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                break;
 
                 case 0:
                     continua = 0;
-                    break;
+                break;
 
                 default:
                     System.out.println("Opção Inválida, tente novamente.");
-                    break;
+                break;
             }
 
         }while(continua == 1);
